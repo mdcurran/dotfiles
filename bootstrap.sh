c773acbca4715df04d51bf4416a7597f2ce4cbd3
Max Curran <maxcurran96@gmail.com>
2022-11-20 21:04:04 +0000

    Install atuin, shellcheck, and apply linting

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Sets the current directory in the process to the dotfiles directory.
-cd "$(dirname "${BASH_SOURCE[0]}")"
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 # Ensure installation of oh-my-zsh.
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -9,24 +9,24 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 fi
 
 # Create a symlink for .gitmessage and set the contents of ~/.gitmessage as the commit template.
-ln -sf $PWD/.gitmessage $HOME/.gitmessage
-git config --global commit.template $HOME/.gitmessage
+ln -sf "$PWD"/.gitmessage "$HOME"/.gitmessage
+git config --global commit.template "$HOME"/.gitmessage
 
 # Set the default name and email for commits.
 git config --global user.name "Max Curran"
 git config --global user.email "maxcurran96@gmail.com"
 
 # Create symlinks for .aliases and ZSH configuration files.
-ln -sf $PWD/.aliases $HOME/.aliases
-ln -sf $PWD/.zshrc $HOME/.zshrc
-ln -sf $PWD/.zprofile $HOME/.zprofile
+ln -sf "$PWD"/.aliases "$HOME"/.aliases
+ln -sf "$PWD"/.zshrc "$HOME"/.zshrc
+ln -sf "$PWD"/.zprofile "$HOME"/.zprofile
 
 # Source these during bootstrapping to reflect changes in the current shell session.
-source $HOME/.zshrc $HOME/.zprofile
+source "$HOME"/.zshrc "$HOME"/.zprofile
 
 # Create ~/.local/bin if it doesn't exist already, and create the symlink for bm.
-mkdir -p $HOME/.local/bin
-ln -sf $PWD/bm $HOME/.local/bin
+mkdir -p "$HOME"/.local/bin
+ln -sf "$PWD"/bm "$HOME"/.local/bin
 
 # Install and manage any Homebrew-related dependencies.
 bm
