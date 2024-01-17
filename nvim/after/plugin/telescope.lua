02b0dc63ebc86e9143487450ebc87ac904af5565
Max Curran <maxcurran96@gmail.com>
2024-01-17 00:09:38 +0000

    Show hidden files (excluding .git) for find_files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,7 +1,19 @@
 local builtin = require("telescope.builtin")
+local telescope = require("telescope")
+
+telescope.setup{
+    defaults = {
+        sorting_strategy = 'descending',
+    },
+    pickers = {
+        find_files = {
+            find_command = {'rg', '--files', '--hidden', '-g', '!.git'},
+        },
+    },
+}
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<leader>ps", function()
-	builtin.grep_string({ search = vim.fn.input("grep: ") })
+    builtin.grep_string({ search = vim.fn.input("grep: ") })
 end)
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
