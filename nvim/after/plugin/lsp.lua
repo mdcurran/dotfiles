d29a7c2821c0ac6d263acdf7336b8bbe27e32d11
Max Curran <maxcurran96@gmail.com>
2024-01-18 14:49:14 +0000

    Use return character for confirming autocomplete

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -35,7 +35,7 @@ cmp.setup {
     mapping = cmp.mapping.preset.insert {
         ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
         ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<cr>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
     },
 }
